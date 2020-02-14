b1277558b937c312e4dc061af23a5a5b5424f2e3
Yunhee Kang <yunh.kang@gmail.com>
2020-02-14 09:06:26 +0900

    Add files via upload

---
+++ test_pd_df.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+cities = {"name": ["London", "Berlin", "Madrid", "Rome", 
+                   "Paris", "Vienna", "Bucharest", "Hamburg", 
+                   "Budapest", "Warsaw", "Barcelona", 
+                   "Munich", "Milan"],
+          "population": [8615246, 3562166, 3165235, 2874038,
+                         2273305, 1805681, 1803425, 1760433,
+                         1754000, 1740119, 1602386, 1493900,
+                         1350680],
+          "area" : [1572, 891.85, 605.77, 1285, 
+                    105.4, 414.6, 228, 755, 
+                    525.2, 517, 101.9, 310.4, 
+                    181.8]
+}
+
+city_frame = pd.DataFrame(cities, columns=["population", "area"], index=cities["name"])
+print(city_frame)
+
+city_frame["area"] *= 1000
+city_frame.plot()
+plt.show()
+
+city_frame["density"] = city_frame["population"] / city_frame["area"]
+print (city_frame["density"])
